d2f76a86668924ad6d4ca9898997807949f8cbe9
marcus-snx <marcus@cc.snxdao.io>
2024-08-30 15:20:51 +0300

    Cache data and API instance

--- dashboards/sample/views/arbitrum.py
+++ dashboards/sample/views/arbitrum.py
@@ -1,12 +1,22 @@
-import streamlit as st
 from datetime import datetime
+
+import streamlit as st
+
 from dashboards.utils.charts import chart_bars
 
 st.markdown("# Arbitrum")
 
-data = st.session_state.api.get_volume(
-    chain="base_mainnet", start_date=datetime(2024, 6, 1), end_date=datetime(2024, 7, 1)
-)
+
+@st.cache_data
+def fetch_data():
+    return st.session_state.api.get_volume(
+        chain="base_mainnet",
+        start_date=datetime(2024, 6, 1),
+        end_date=datetime(2024, 7, 1),
+    )
+
+
+data = fetch_data()
 
 st.write(data)
 

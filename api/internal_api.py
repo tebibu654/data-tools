9930a756d6e981fe289d202b440c3ffb03d67985
marcus-snx <marcus@cc.snxdao.io>
2024-09-05 20:03:22 +0300

    Update core and perps API data methods

--- api/internal_api.py
+++ api/internal_api.py
@@ -41,6 +41,8 @@ def get_connection(db_config):
 
 
 class SynthetixAPI:
+    SUPPORTED_CHAINS = ["Arbitrum", "Base"]
+
     def __init__(
         self, db_config: dict, environment: str = "prod", streamlit: bool = True
     ):
@@ -77,6 +79,23 @@ class SynthetixAPI:
         finally:
             connection.close()
 
+    def _get_chain_label(self, chain: str) -> str:
+        """
+        Get the SQL-friendly chain label string.
+
+        Args:
+            chain (str): Chain to query (e.g., 'Arbitrum', 'Base', 'All')
+
+        Returns:
+            str: SQL-friendly chain label string
+        """
+        if chain == "All":
+            return ", ".join(f"'{c}'" for c in self.SUPPORTED_CHAINS)
+        elif chain in self.SUPPORTED_CHAINS:
+            return f"'{chain}'"
+        else:
+            raise ValueError(f"Unsupported chain: {chain}")
+
     # queries
     def get_volume(
         self,
@@ -109,103 +128,171 @@ class SynthetixAPI:
         with self.get_connection() as conn:
             return pd.read_sql_query(query, conn)
 
-    def get_tvl_by_chain(
+    def get_core_stats_by_chain(
         self,
         start_date: datetime,
         end_date: datetime,
+        chain: str = "All",
     ) -> pd.DataFrame:
         """
-        Get Total Value Locked (TVL) data for all chains.
+        Get core stats by chain.
 
         Args:
             start_date (datetime): Start date for the query
             end_date (datetime): End date for the query
-            resolution (str): Data resolution ('daily' or 'hourly')
+            chain (str): Chain to query (e.g., 'arbitrum_mainnet', 'base_mainnet')
 
         Returns:
-            pandas.DataFrame: TVL data with columns 'ts', 'chain', 'collateral_value', 'cumulative_pnl'
+            pandas.DataFrame: Core stats with columns 'ts', 'chain', 'collateral_value'
         """
+        label = self._get_chain_label(chain)
         query = f"""
+        WITH tvl AS (
+            SELECT
+                ts,
+                'Arbitrum' AS chain,
+                SUM(collateral_value) AS collateral_value
+            FROM {self.environment}_arbitrum_mainnet.fct_core_apr_arbitrum_mainnet AS apr
+            GROUP BY ts, chain
+
+            UNION ALL
+
+            SELECT
+                ts,
+                'Base' AS chain,
+                SUM(collateral_value) AS collateral_value
+            FROM {self.environment}_base_mainnet.fct_core_apr_base_mainnet AS apr
+            GROUP BY ts, chain
+
+            ORDER BY ts
+        )
+
         SELECT
             ts,
-            'Arbitrum' AS chain,
-            SUM(collateral_value) AS collateral_value,
-            SUM(cumulative_pnl) AS cumulative_pnl
-        FROM {self.environment}_arbitrum_mainnet.fct_core_apr_arbitrum_mainnet AS apr
-        WHERE ts >= '{start_date}' and ts <= '{end_date}'
-        GROUP BY ts, chain
+            chain,
+            collateral_value
+        FROM tvl
+        WHERE 
+            ts >= '{start_date}' and ts <= '{end_date}'
+            AND chain in ({label})
 
-        UNION ALL
-
-        SELECT
-            ts,
-            'Base' AS chain,
-            SUM(collateral_value) AS collateral_value,
-            SUM(cumulative_pnl) AS cumulative_pnl
-        FROM {self.environment}_base_mainnet.fct_core_apr_base_mainnet AS apr
-        WHERE ts >= '{start_date}' and ts <= '{end_date}'
-        GROUP BY ts, chain
-        ORDER BY ts
         """
         with self.get_connection() as conn:
             return pd.read_sql_query(query, conn)
 
-    def get_tvl_by_collateral(
+    def get_core_stats_by_collateral(
         self,
         start_date: datetime,
         end_date: datetime,
-        resolution: str = "daily",
         chain: str = "All",
+        resolution: str = "28d",
     ) -> pd.DataFrame:
         """
-        Get Total Value Locked (TVL) data broken down by collateral type for all chains.
-        This function retrieves detailed TVL information including collateral value, debt,
-        PNL, rewards, issuance, and APR for each collateral type on both Arbitrum and Base chains.
+        Get core stats by collateral.
+        This function retrieves detailed core stats including
+        collateral value, debt, PNL, rewards, issuance, and APR
+        for each collateral type on both Arbitrum and Base chains.
 
         Args:
             start_date (datetime): Start date for the query
             end_date (datetime): End date for the query
+            chain (str): Chain to query (e.g., 'arbitrum_mainnet', 'base_mainnet')
             resolution (str): Data resolution ('daily' or 'hourly')
 
         Returns:
             pandas.DataFrame: TVL data with columns:
-                'ts', 'label', 'chain', 'collateral_value'
+                'ts', 'label', 'chain', 'collateral_value', 'apr'
         """
-        if chain == "All":
-            label = "'Arbitrum', 'Base'"
-        else:
-            label = f"'{chain}'"
+        label = self._get_chain_label(chain)
         query = f"""
         WITH tvl AS (
-        SELECT 
-            ts,
-            CONCAT(coalesce(tk.token_symbol, collateral_type), ' (Arbitrum)') as label,
-            'Arbitrum' as chain,
-            collateral_value
-        FROM {self.environment}_arbitrum_mainnet.fct_core_apr_arbitrum_mainnet apr
-        LEFT JOIN {self.environment}_seeds.arbitrum_mainnet_tokens tk on lower(apr.collateral_type) = lower(tk.token_address)
+            SELECT 
+                ts,
+                CONCAT(coalesce(tk.token_symbol, collateral_type), ' (Arbitrum)') as label,
+                'Arbitrum' as chain,
+                collateral_value,
+                apr_{resolution} as apr,
+                apr_{resolution}_rewards as apr_rewards
+            FROM {self.environment}_arbitrum_mainnet.fct_core_apr_arbitrum_mainnet apr
+            LEFT JOIN {self.environment}_seeds.arbitrum_mainnet_tokens tk on lower(apr.collateral_type) = lower(tk.token_address)
 
-        UNION ALL
+            UNION ALL
 
-        SELECT 
-            ts,
-            CONCAT(coalesce(tk.token_symbol, collateral_type), ' (Base)') as label,
-            'Base' as chain,
-            collateral_value
-        FROM {self.environment}_base_mainnet.fct_core_apr_base_mainnet apr
-        LEFT JOIN {self.environment}_seeds.base_mainnet_tokens tk on lower(apr.collateral_type) = lower(tk.token_address)
+            SELECT 
+                ts,
+                CONCAT(coalesce(tk.token_symbol, collateral_type), ' (Base)') as label,
+                'Base' as chain,
+                collateral_value,
+                apr_{resolution} as apr,
+                apr_{resolution}_rewards as apr_rewards
+            FROM {self.environment}_base_mainnet.fct_core_apr_base_mainnet apr
+            LEFT JOIN {self.environment}_seeds.base_mainnet_tokens tk on lower(apr.collateral_type) = lower(tk.token_address)
         )
 
         SELECT 
             ts,
             label,
             chain,
-            collateral_value
+            collateral_value,
+            apr,
+            apr_rewards
         FROM tvl
         WHERE 
-            chain in ({label})
-            AND ts >= '{start_date}' and ts <= '{end_date}'
+            ts >= '{start_date}' and ts <= '{end_date}'
+            AND chain in ({label})
         ORDER BY ts
+        """
+        with self.get_connection() as conn:
+            return pd.read_sql_query(query, conn)
+
+    def get_perps_stats_by_chain(
+        self,
+        start_date: datetime,
+        end_date: datetime,
+        chain: str = "All",
+        resolution: str = "daily",
+    ) -> pd.DataFrame:
+        """
+        Get perps stats by chain.
+
+        Args:
+            start_date (datetime): Start date for the query
+            end_date (datetime): End date for the query
+            chain (str): Chain to query (e.g., 'arbitrum_mainnet', 'base_mainnet')
+
+        Returns:
+            pandas.DataFrame: Perps stats with columns:
+                'ts', 'volume', 'exchange_fees'
+        """
+        label = self._get_chain_label(chain)
+        query = f"""
+        WITH perps_stats AS (
+            SELECT
+                ts,
+                'Arbitrum' as chain,
+                volume,
+                exchange_fees
+            FROM {self.environment}_arbitrum_mainnet.fct_perp_stats_{resolution}_arbitrum_mainnet
+
+            UNION ALL
+
+            SELECT
+                ts,
+                'Base' as chain,
+                volume,
+                exchange_fees
+            FROM {self.environment}_base_mainnet.fct_perp_stats_{resolution}_base_mainnet
+        )
+
+        SELECT
+            ts,
+            chain,
+            volume,
+            exchange_fees
+        FROM perps_stats
+        WHERE
+            ts >= '{start_date}' and ts <= '{end_date}'
+            AND chain in ({label})
         """
         with self.get_connection() as conn:
             return pd.read_sql_query(query, conn)

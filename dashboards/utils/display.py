cbf55f462ac0812432e2f0af4b852925f96f24bf
Troy <troy.burmeister@gmail.com>
2024-10-16 12:58:23 -0600

    svg update

--- dashboards/utils/display.py
+++ dashboards/utils/display.py
@@ -4,11 +4,6 @@ from pathlib import Path
 import urllib.parse
 
 
-def encode_svg_for_css(svg):
-    svg_one_line = svg.replace("\n", " ").replace('"', "'")
-    return urllib.parse.quote(svg_one_line)
-
-
 def sidebar_logo():
     LOGO_URL = "dashboards/static/logo.png"
     HEIGHT = 28
@@ -30,13 +25,7 @@ def sidebar_logo():
 
 
 def sidebar_icon():
-    burger_svg = """<svg width="18" height="15" viewBox="0 0 18 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M0.512695 1.31128C0.512695 0.897066 0.848482 0.561279 1.2627 0.561279H16.2627C16.6769 0.561279 17.0127 0.897066 17.0127 1.31128C17.0127 1.72549 16.6769 2.06128 16.2627 2.06128H1.2627C0.848482 2.06128 0.512695 1.72549 0.512695 1.31128Z" fill="white"/>
-<path d="M0.512695 7.31128C0.512695 6.89707 0.848482 6.56128 1.2627 6.56128H16.2627C16.6769 6.56128 17.0127 6.89707 17.0127 7.31128C17.0127 7.72549 16.6769 8.06128 16.2627 8.06128H1.2627C0.848482 8.06128 0.512695 7.72549 0.512695 7.31128Z" fill="white"/>
-<path d="M1.2627 12.5613C0.848482 12.5613 0.512695 12.8971 0.512695 13.3113C0.512695 13.7255 0.848482 14.0613 1.2627 14.0613H16.2627C16.6769 14.0613 17.0127 13.7255 17.0127 13.3113C17.0127 12.8971 16.6769 12.5613 16.2627 12.5613H1.2627Z" fill="white"/>
-</svg>
-    """
-    encoded_svg = encode_svg_for_css(burger_svg)
+    burger_svg = """%3Csvg%20width%3D%2718%27%20height%3D%2715%27%20viewBox%3D%270%200%2018%2015%27%20fill%3D%27none%27%20xmlns%3D%27http%3A//www.w3.org/2000/svg%27%3E%20%3Cpath%20d%3D%27M0.512695%201.31128C0.512695%200.897066%200.848482%200.561279%201.2627%200.561279H16.2627C16.6769%200.561279%2017.0127%200.897066%2017.0127%201.31128C17.0127%201.72549%2016.6769%202.06128%2016.2627%202.06128H1.2627C0.848482%202.06128%200.512695%201.72549%200.512695%201.31128Z%27%20fill%3D%27white%27/%3E%20%3Cpath%20d%3D%27M0.512695%207.31128C0.512695%206.89707%200.848482%206.56128%201.2627%206.56128H16.2627C16.6769%206.56128%2017.0127%206.89707%2017.0127%207.31128C17.0127%207.72549%2016.6769%208.06128%2016.2627%208.06128H1.2627C0.848482%208.06128%200.512695%207.72549%200.512695%207.31128Z%27%20fill%3D%27white%27/%3E%20%3Cpath%20d%3D%27M1.2627%2012.5613C0.848482%2012.5613%200.512695%2012.8971%200.512695%2013.3113C0.512695%2013.7255%200.848482%2014.0613%201.2627%2014.0613H16.2627C16.6769%2014.0613%2017.0127%2013.7255%2017.0127%2013.3113C17.0127%2012.8971%2016.6769%2012.5613%2016.2627%2012.5613H1.2627Z%27%20fill%3D%27white%27/%3E%20%3C/svg%3E%20%20%20%20%20"""
     custom_css = f"""
     <style>
         [data-testid="stSidebarCollapseButton"] button svg {{
@@ -48,7 +37,7 @@ def sidebar_icon():
             display: block;
             width: 24px;
             height: 24px;
-            background-image: url("data:image/svg+xml,{encoded_svg}");
+            background-image: url("data:image/svg+xml,{burger_svg}");
             background-size: contain;
             background-repeat: no-repeat;
             background-position: center center;
@@ -63,7 +52,7 @@ def sidebar_icon():
             display: block;
             width: 24px;
             height: 24px;
-            background-image: url("data:image/svg+xml,{encoded_svg}");
+            background-image: url("data:image/svg+xml,{burger_svg}");
             background-size: contain;
             background-repeat: no-repeat;
             background-position: center center;
